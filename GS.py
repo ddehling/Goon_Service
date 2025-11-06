c7f2989627c4e88f9bf7252037a7a124ad82528f
Dietrich Dehlinger <ddehling@gmail.com>
2025-11-05 19:36:54 -0800

    Jeffs updates

--- GS.py
+++ GS.py
@@ -77,9 +77,58 @@ if __name__ == "__main__":
     scheduler.setup_visualizer(False) 
     # Start with summer bloom weather
    
-
-    env_system.scheduler.schedule_event(0, 60, GS_tingles)  # noqa: F405
-    #env_system.scheduler.schedule_event(10, 20, GS_hot_tub)  # noqa: F405
+    curr = 0
+    def schedule_next(seconds, func, overlap=0):
+        global curr
+        end = curr + seconds
+        beginning = curr
+        if overlap and curr != 0:
+            beginning = beginning - overlap
+            end = end + overlap
+        #print(f"Scheduling {func.__name__} {beginning} => {end}")
+        # comment this out so we can do it without the intervening function
+        env_system.scheduler.schedule_event(beginning, end, func)
+        print(f"env_system.scheduler.schedule_event({beginning}, {end}, {func.__name__})")
+        curr = end
+    def get_active_states():
+        return ", ".join(event.action.__name__ for event in scheduler.active_events)
+    funcs = [GS_tingles, GS_rage_lightning, GS_curious_playful, GS_forest,
+                 GS_blood_flow, GS_blink_fade, GS_hypnotic_spiral, GS_rage_lightning, GS_shibari,
+                 GS_sunrise]
+    f2 = list(funcs)
+    f3 = list(funcs)
+    import random
+    random.shuffle(funcs)
+    random.shuffle(f2)
+    random.shuffle(f3)
+    #for func in funcs + f2 + f3:
+    #    schedule_next(10, func, 1)
+    # schedule_next(30,GS_tingles)  # noqa: F405
+    # schedule_next(40, GS_hot_tub, 10)  # noqa: F405
+    schedule_next(60, GS_tingles)
+    schedule_next(60, GS_rage_lightning)
+    schedule_next(60, GS_blink_fade)
+    schedule_next(60, GS_sunrise)
+    schedule_next(60, GS_tingles)
+    schedule_next(60, GS_rage_lightning)
+    schedule_next(60, GS_blink_fade)
+    schedule_next(60, GS_sunrise)
+    schedule_next(60, GS_tingles)
+    schedule_next(60, GS_rage_lightning)
+    schedule_next(60, GS_blink_fade)
+    schedule_next(60, GS_sunrise)
+    #env_system.scheduler.schedule_event(0, 10, GS_tingles)
+    #env_system.scheduler.schedule_event(10, 20, GS_rage_lightning)
+    #env_system.scheduler.schedule_event(20, 30, GS_blink_fade)
+    #env_system.scheduler.schedule_event(30, 40, GS_sunrise)
+    #env_system.scheduler.schedule_event(40, 50, GS_tingles)
+    #env_system.scheduler.schedule_event(50, 60, GS_rage_lightning)
+    #env_system.scheduler.schedule_event(60, 70, GS_blink_fade)
+    #env_system.scheduler.schedule_event(70, 80, GS_sunrise)
+    #env_system.scheduler.schedule_event(80, 90, GS_tingles)
+    #env_system.scheduler.schedule_event(90, 100, GS_rage_lightning)
+    #env_system.scheduler.schedule_event(100, 110, GS_blink_fade)
+    #env_system.scheduler.schedule_event(110, 120, GS_sunrise)
     #env_system.scheduler.schedule_event(0, 60, GS_hypnotic_spiral)
     #env_system.scheduler.schedule_event(0, 60, GS_forest)
     #env_system.scheduler.schedule_event(0, 300, GS_tingles)
@@ -99,7 +148,7 @@ if __name__ == "__main__":
             time.sleep(sleep_time)
 
             # Print stats if needed
-            print(["%.2f" % (1/(time.perf_counter()-lasttime)), "%.2f" % len(scheduler.active_events), len(scheduler.event_queue),"%.3f" %((lasttime-first_time)/3600)])
+            print(["%.2f" % (1/(time.perf_counter()-lasttime)), "%.2f" % len(scheduler.active_events), len(scheduler.event_queue),"%.3f" %((lasttime-first_time)/3600), get_active_states()])
             lasttime = time.perf_counter()
 
     except KeyboardInterrupt:
