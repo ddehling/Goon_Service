303f0b13edb6edb331be5be274f30b9f91be308a
Dietrich Dehlinger <ddehling@gmail.com>
2025-11-09 10:17:58 -0800

    set audio volume to 100%

--- GS_full.py
+++ GS_full.py
@@ -131,7 +131,7 @@ if __name__ == "__main__":
     
     try:
         env_system.VLC.launch_with_fallback(disable_keyboard=True, autoplay=False)
-        env_system.VLC.set_volume(512)
+        env_system.VLC.set_volume(256)
         
         while True:
             # Switch to video and ensure fullscreen
