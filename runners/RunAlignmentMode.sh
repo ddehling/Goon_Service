a01cb1e98590606994d37e3446afda824b3c9f85
Dietrich Dehlinger <ddehling@gmail.com>
2025-11-09 00:27:03 -0800

    run alignment mode

--- runners/RunAlignmentMode.sh
+++ runners/RunAlignmentMode.sh
@@ -4,7 +4,7 @@
 set -eo pipefail
 set -x
 SCRIPT_DIR="$(dirname $(realpath $0))"
-FILE_TO_RUN="GS_full.py"
+FILE_TO_RUN="align_tool.py"
 
 # x-terminal-emulator causes this to pop up in new window so we can quit
 x-terminal-emulator -e "${SCRIPT_DIR}/run_python_script.sh $FILE_TO_RUN"
